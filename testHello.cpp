27f0de0f21a2a7a50bcc61beeda2bc450e1dda8c
jxl <whutjxl@example.com>
2016-01-12 22:08:23 +0800

    create testHello.cpp

---
+++ testHello.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i;
+    int n;
+    cin>>n;
+    for(i=0;i<n;++i)
+    {
+        cout<<"Hello C++ program   "<<i<<endl;
+    }
+    return 0;
+}
